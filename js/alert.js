cb7af7865fe6bcbad151ab8a218b7b330a1f5f52
Steve Ngo <57868614+SteveNgo96@users.noreply.github.com>
2019-11-17 11:20:13 -0500

    Add files via upload

---
+++ js/alert.js
@@ -0,0 +1,12 @@
+function myalert(){
+		var m1=document.getElementById('fName').value;
+		var m2=document.getElementById('lName').value;
+		var m3=document.getElementById('consoles').value;
+		alert("Thank you for you input. So your name is " +m1+ " "+m2+ ". You regulary play on the " +m3+
+		".\n Please wait till your infomation to be process");
+	}
+/* Steve Ngo
+November 10, 2019
+My Function List
+Task 2
+*/
